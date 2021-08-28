fff12efe8d67df9ade31565b326c39e9db1718d7
son <sonntuet1997@gmail.com>
2021-08-29 02:30:46 +0700

    update

--- error.go
+++ error.go
@@ -3,6 +3,7 @@ package utils
 import "golang.org/x/xerrors"
 
 var (
-	ErrAuthBodyInvalid       = xerrors.New("ERROR.AUTH.ROLE.INVALID_BODY")
-	ErrAuthActionTypeInvalid = xerrors.New("ERROR.AUTH.ROLE.INVALID_ACTION_TYPE")
+	ErrAuthBodyInvalid       = xerrors.New("ERROR.AUTH.INVALID_BODY")
+	ErrAuthActionTypeInvalid = xerrors.New("ERROR.AUTH.INVALID_ACTION_TYPE")
+	ErrAuthParseModelFail    = xerrors.New("ERROR.AUTH.PARSE_MODEL_FAIL")
 )
