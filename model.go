77f592055d00a2123fa116cbbb9427aa34a4001e
son <sonntuet1997@gmail.com>
2021-08-29 01:48:01 +0700

    update

--- model.go
+++ model.go
@@ -1,14 +1,12 @@
 package utils
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 )
 
 type ModelBase struct {
 	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey;column:id" json:"id"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime:nano" json:"updatedAt"`
-	CreatedAt time.Time `gorm:"autoCreateTime:nano" json:"createdAt"`
-	DeletedAt time.Time `gorm:"autoDeleteTime:nano" json:"deletedAt"`
+	UpdatedAt int64     `gorm:"autoUpdateTime" json:"updatedAt"`
+	CreatedAt int64     `gorm:"autoCreateTime" json:"createdAt"`
+	DeletedAt int64     `gorm:"autoDeleteTime" json:"deletedAt"`
 }
