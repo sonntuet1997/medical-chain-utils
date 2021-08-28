2cba734e0bf8dbb2020ee4a300e6bb091364c0e5
son <sonntuet1997@gmail.com>
2021-08-29 02:33:21 +0700

    update

--- error.go
+++ error.go
@@ -6,4 +6,5 @@ var (
 	ErrAuthBodyInvalid       = xerrors.New("ERROR.AUTH.INVALID_BODY")
 	ErrAuthActionTypeInvalid = xerrors.New("ERROR.AUTH.INVALID_ACTION_TYPE")
 	ErrAuthParseModelFail    = xerrors.New("ERROR.AUTH.PARSE_MODEL_FAIL")
+	ErrAuthTimestampInvalid  = xerrors.New("ERROR.AUTH.INVALID_TIMESTAMP")
 )
