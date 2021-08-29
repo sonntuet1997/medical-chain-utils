b50ec72984ec10b8636afcc351ad0125f93dd638
son <sonntuet1997@gmail.com>
2021-08-29 18:00:01 +0700

    aa

--- cockroach/service.go
+++ cockroach/service.go
@@ -12,7 +12,7 @@ type CommonDataService interface {
 }
 
 type CDBService struct {
-	Db         *ExtendGorm
+	Db         *gorm.DB
 	Interfaces []interface{}
 }
 
@@ -29,7 +29,7 @@ func NewCDBConnection(dsn string) (*gorm.DB, error) {
 }
 
 func (c *CDBService) Close() error {
-	d, err := c.Db.DB.DB()
+	d, err := c.Db.DB()
 	if err != nil {
 		return err
 	}
