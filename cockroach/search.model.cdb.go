6422edcc6783e2fc5587a606c1571d472d8f3998
son <sonntuet1997@gmail.com>
2021-08-29 12:07:01 +0700

    aa

--- cockroach/search.model.cdb.go
+++ cockroach/search.model.cdb.go
@@ -6,6 +6,44 @@ import (
 	"strings"
 )
 
+type SearchModelCDB struct {
+	*gorm.DB
+	common.DefaultSearchModel
+}
+
+func (s *SearchModelCDB) ApplyPagination() *SearchModelCDB {
+	db := s.DB
+	if s.DefaultSearchModel.Limit > 0 {
+		db = db.Limit(s.DefaultSearchModel.Limit)
+	}
+	if s.DefaultSearchModel.Skip > 0 {
+		db = db.Offset(s.DefaultSearchModel.Skip)
+	}
+	return &SearchModelCDB{
+		DB:                 db,
+		DefaultSearchModel: s.DefaultSearchModel,
+	}
+}
+
+func (s *SearchModelCDB) ApplySort() *SearchModelCDB {
+	db := s.DB
+	if s.DefaultSearchModel.OrderBy != "" {
+		orderByList := strings.Fields(s.DefaultSearchModel.OrderBy)
+		orderTypeList := strings.Fields(s.DefaultSearchModel.OrderType)
+		for i, orderBy := range orderByList {
+			orderType := "asc"
+			if orderTypeList[i] != "asc" {
+				orderType = "desc"
+			}
+			db = db.Order(orderBy + " " + orderType)
+		}
+	}
+	return &SearchModelCDB{
+		DB:                 db,
+		DefaultSearchModel: s.DefaultSearchModel,
+	}
+}
+
 func ApplyPaginationCDB(defaultSearchModel common.DefaultSearchModel, db *gorm.DB) *gorm.DB {
 	if defaultSearchModel.Limit > 0 {
 		db = db.Limit(defaultSearchModel.Limit)
