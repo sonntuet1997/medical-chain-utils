e0537768464f3578f3a6e0ced57fc59ca5a0fcef
Vu Nguyen <aqaurius6666@gmail.com>
2021-08-31 17:45:51 +0700

    feat: not login yet

--- common/error.go
+++ common/error.go
@@ -7,4 +7,5 @@ var (
 	ErrAuthActionTypeInvalid = xerrors.New("ERROR.AUTH.INVALID_ACTION_TYPE")
 	ErrAuthParseModelFail    = xerrors.New("ERROR.AUTH.PARSE_MODEL_FAIL")
 	ErrAuthTimestampInvalid  = xerrors.New("ERROR.AUTH.INVALID_TIMESTAMP")
+	ErrNotLoginYet           = xerrors.New("ERROR.AUTH.NOT_LOGIN_YET")
 )
