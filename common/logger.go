01fdb917e63fa07b29c91187ef46383a534fe932
son <sonntuet1997@gmail.com>
2021-09-02 01:30:00 +0700

    π

---
+++ common/logger.go
@@ -0,0 +1,30 @@
+package common
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+	"os"
+	"time"
+)
+
+func InitLogger(appCtx *cli.Context) *logrus.Logger {
+	logger := logrus.New()
+	host, _ := os.Hostname()
+	isVerbose := appCtx.Bool("verbose")
+	logger.Level = logrus.InfoLevel
+	if isVerbose {
+		logger.Level = logrus.DebugLevel
+	}
+	logger.Formatter = &logrus.JSONFormatter{
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "timestamp",
+			logrus.FieldKeyLevel: "severity",
+			logrus.FieldKeyMsg:   "message",
+			"version":            appCtx.String("runtime-version"),
+			"host":               host,
+		},
+		TimestampFormat: time.RFC3339Nano,
+	}
+	logger.Out = os.Stdout
+	return logger
+}
