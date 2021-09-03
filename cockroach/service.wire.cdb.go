2eef6f65aaf5d9ec0dde2d94f70f787c06c66dc4
son <sonntuet1997@gmail.com>
2021-09-03 16:09:46 +0700

    Update Go wire

---
+++ cockroach/service.wire.cdb.go
@@ -0,0 +1,16 @@
+//go:build wireinject
+// +build wireinject
+
+package cockroach
+
+import (
+	"context"
+	"github.com/google/wire"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+func InitCDBService(ctx context.Context, logger *logrus.Logger, db *gorm.DB, interfaces []interface{}) (*CDBService, error) {
+	wire.Build(wire.Struct(new(CDBService), "Db", "Logger", "Context", "Interfaces"))
+	return &CDBService{}, nil
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.16
 require (
 	github.com/ethereum/go-ethereum v1.10.7
 	github.com/google/uuid v1.3.0
+	github.com/google/wire v0.5.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20210903071746-97244b99971b // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.9
