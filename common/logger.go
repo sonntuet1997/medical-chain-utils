b4522c28a4793f1528cc869d8b8a43ad57a4992c
son <sonntuet1997@gmail.com>
2021-09-04 06:21:06 +0700

    Update Go wire

--- common/logger.go
+++ common/logger.go
@@ -4,28 +4,57 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 	"os"
+	"strings"
 	"time"
+)
+
+var (
+	LoggerFlag = []cli.Flag{
+		&cli.StringFlag{
+			Name:    "log-level",
+			EnvVars: []string{"LOG_LEVEL"},
+			Usage:   "Log level: (panic|fatal|error|warn|warning|info|debug|trace)",
+			Value:   "info",
+		},
+		&cli.StringFlag{
+			Name:    "log-format",
+			EnvVars: []string{"LOG_FORMAT"},
+			Usage:   "Log format: (plain|json)",
+			Value:   "json",
+		},
+	}
 )
 
 func InitLogger(appCtx *cli.Context) *logrus.Logger {
 	logger := logrus.New()
 	host, _ := os.Hostname()
-	isVerbose := appCtx.Bool("verbose")
-	logger.Level = logrus.InfoLevel
-	if isVerbose {
-		logger.Level = logrus.DebugLevel
-	}
-	logger.Formatter = &logrus.JSONFormatter{
-		FieldMap: logrus.FieldMap{
-			logrus.FieldKeyTime:  "timestamp",
-			logrus.FieldKeyLevel: "severity",
-			logrus.FieldKeyMsg:   "message",
-			"version":            appCtx.String("runtime-version"),
-			"host":               host,
-		},
-		TimestampFormat: time.RFC3339Nano,
-	}
+	logLevel := appCtx.String("log-level")
+	level, err := logrus.ParseLevel(logLevel)
 	logger.Out = os.Stdout
+	if err != nil {
+		logger.Fatalf("Unknown log-level type: %s", logLevel)
+		return logger
+	}
+	logger.Level = level
+	logFormat := appCtx.String("log-format")
+	switch strings.ToLower(logFormat) {
+	case "json":
+		logger.Formatter = &logrus.JSONFormatter{
+			FieldMap: logrus.FieldMap{
+				logrus.FieldKeyTime:  "timestamp",
+				logrus.FieldKeyLevel: "severity",
+				logrus.FieldKeyMsg:   "message",
+				"version":            appCtx.String("runtime-version"),
+				"host":               host,
+			},
+			TimestampFormat: time.RFC3339Nano,
+		}
+	case "plain":
+		logger.Formatter = &logrus.TextFormatter{}
+	default:
+		logger.Fatalf("Unknown log-format type: %s", logFormat)
+		return logger
+	}
 	return logger
 }
 func InitLoggerWithoutCLIContext() *logrus.Logger {
