8cc2adaae2052ec59f55797dc4b79b74dae2634c
Vu Nguyen <aqaurius6666@gmail.com>
2021-09-05 14:01:47 +0700

    feat: parse erorr

--- common/error.go
+++ common/error.go
@@ -1,6 +1,9 @@
 package common
 
-import "golang.org/x/xerrors"
+import (
+	"golang.org/x/xerrors"
+	"google.golang.org/grpc/status"
+)
 
 var (
 	ErrAuthBodyInvalid       = xerrors.New("ERROR.AUTH.INVALID_BODY")
@@ -9,4 +12,13 @@ var (
 	ErrAuthTimestampInvalid  = xerrors.New("ERROR.AUTH.INVALID_TIMESTAMP")
 	ErrNotLoginYet           = xerrors.New("ERROR.AUTH.NOT_LOGIN_YET")
 	ErrMissingCertificate    = xerrors.New("ERROR.AUTH.MISSING_CERTIFICATE")
+	ErrParseError            = xerrors.New("ERROR.COMMON.PARSE_ERROR_FAIL")
 )
+
+func ParseGrpcError(err error) error {
+	s, ok := status.FromError(err)
+	if !ok {
+		return ErrParseError
+	}
+	return xerrors.New(s.Message())
+}
