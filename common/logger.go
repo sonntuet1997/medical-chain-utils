e7c57291a632ce4cc925cab92050a8ee2fad0791
son <sonntuet1997@gmail.com>
2021-09-05 18:41:15 +0700

    Update Go wire

--- common/logger.go
+++ common/logger.go
@@ -9,6 +9,51 @@ import (
 )
 
 var (
+	CommonGRPCFlag = []cli.Flag{
+		&cli.StringFlag{
+			Name:    "runtime-version",
+			EnvVars: []string{"RUNTIME_VERSION"},
+			Value:   "v1.0.0",
+		},
+		&cli.IntFlag{
+			Name:    "grpc-port",
+			Value:   8080,
+			EnvVars: []string{"GRPC_PORT"},
+			Usage:   "The port for exposing the gRPC endpoints for accessing",
+		},
+		&cli.IntFlag{
+			Name:    "http-port",
+			Value:   80,
+			EnvVars: []string{"HTTP_PORT"},
+			Usage:   "The port for exposing the api endpoints for accessing",
+		},
+		&cli.IntFlag{
+			Name:    "pprof-port",
+			Value:   6060,
+			EnvVars: []string{"PPROF_PORT"},
+			Usage:   "The port for exposing pprof endpoints",
+		},
+		&cli.BoolFlag{
+			Name:    "disable-tracing",
+			EnvVars: []string{"DISABLE_TRACING"},
+			Usage:   "disable-tracing",
+		},
+		&cli.BoolFlag{
+			Name:    "disable-profiler",
+			EnvVars: []string{"DISABLE_PROFILER"},
+			Usage:   "disable-profiler",
+		},
+		&cli.BoolFlag{
+			Name:    "disable-stats",
+			EnvVars: []string{"DISABLE_STATS"},
+			Usage:   "disable-stats",
+		},
+		&cli.BoolFlag{
+			Name:    "allow-kill",
+			EnvVars: []string{"ALLOW_KILL"},
+			Usage:   "allow remote request to kill server",
+		},
+	}
 	LoggerFlag = []cli.Flag{
 		&cli.StringFlag{
 			Name:    "log-level",
