a7f20366de42c7efbe01937de139faff43e5764f
Vu Nguyen <aqaurius6666@gmail.com>
2021-09-14 12:08:52 +0700

    fix: messaging

--- messaging/kafka.go
+++ messaging/kafka.go
@@ -7,14 +7,14 @@ import (
 )
 
 type KafkaOptions struct {
-	addr  string
-	topic string
+	Addr  string
+	Topic string
 }
 
 func ConnectReader(opts KafkaOptions) *kafka.Reader {
 	return kafka.NewReader(kafka.ReaderConfig{
-		Brokers:  []string{opts.addr},
-		Topic:    opts.topic,
+		Brokers:  []string{opts.Addr},
+		Topic:    opts.Topic,
 		MinBytes: 1,
 		MaxBytes: 10e6,
 	})
@@ -22,13 +22,13 @@ func ConnectReader(opts KafkaOptions) *kafka.Reader {
 
 func ConnectWriter(opts KafkaOptions) *kafka.Writer {
 	return kafka.NewWriter(kafka.WriterConfig{
-		Topic:        opts.topic,
+		Topic:        opts.Topic,
 		Balancer:     &kafka.LeastBytes{},
 		WriteTimeout: 5 * time.Second,
 		MaxAttempts:  4,
 		Async:        true,
 		BatchSize:    1,
-		Brokers:      []string{opts.addr},
+		Brokers:      []string{opts.Addr},
 	})
 
 }
