a14443271184c32ff1672c00effef8fa515b5848
Vu Nguyen <aqaurius6666@gmail.com>
2021-09-21 22:11:57 +0700

    feat: ptr val

--- main.go
+++ main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
 
 	"github.com/sonntuet1997/medical-chain-utils/cryptography"
 )
 
 func main() {
-	priv, _, err := cryptography.GenerateKeyPair(nil)
-	if err != nil {
-		log.Println(err)
+	pk := "A0IAZ/+cyKCNhxYELQAraHhIkI+T+WWaJLpcYjMK0a+s"
+	js := `{"data":{"providerId":"dsadada","name":"32131231","manager":"@#@#!##$%^&*()","mail":"3dajfajf@gmail.com","phone":"@#$%^&*()","description":"@#$%^&*()","_actionType":"POST_API-ADMIN-PROVIDER","_timestamp":"2021-09-21T13:23:47.628Z"},"_signature":"9iReEsP9LquB24Ysy0tWD3DczDoDk6MpKxfQvAYa4lZGgspisGzgB2ZAoHqd2DhsIsLE+kPN3nx2dW974yqT6Q=="}`
+	type base struct {
+		Data      json.RawMessage `json:"data"`
+		Signature string          `json:"_signature"`
 	}
-	// data := map[string]interface{}{"id": "HTpLb7OwuuAXTsVdGRmvn"}
-	sig, err := cryptography.SignMessage("HTpLb7OwuuAXTsVdGRmvn", priv)
-	if err != nil {
-		log.Println(err)
-	}
-	bsig := cryptography.ConvertBytesToBase64(sig)
-	str := "HTpLb7OwuuAXTsVdGRmvn" + "," + bsig
-	fmt.Printf("[]byte(str): %v\n", []byte(str))
-	fmt.Printf("len([]byte(str)): %v\n", len([]byte(str)))
+	var a base
+	json.Unmarshal([]byte(js), &a)
+	bpk, _ := cryptography.ConvertBase64ToBytes(pk)
+	bsig, _ := cryptography.ConvertBase64ToBytes(a.Signature)
+	fmt.Printf("bpk: %v\n", bpk)
+	fmt.Printf("bsig: %v\n", bsig)
 }
