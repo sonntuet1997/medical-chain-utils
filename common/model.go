b9de59426880076958e7b9a1a02f11ae281e6f2c
Vu Nguyen <54926438+aquarius6666@users.noreply.github.com>
2021-10-26 10:35:38 +0700

    Update model.go

--- common/model.go
+++ common/model.go
@@ -6,7 +6,7 @@ import (
 
 type ModelBase struct {
 	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey;column:id" json:"id"`
-	UpdatedAt int64     `gorm:"autoUpdateTime" json:"updatedAt"`
-	CreatedAt int64     `gorm:"autoCreateTime" json:"createdAt"`
-	DeletedAt int64     `gorm:"autoDeleteTime" json:"deletedAt"`
+	UpdatedAt int64     `gorm:"autoUpdateTime:milli" json:"updatedAt"`
+	CreatedAt int64     `gorm:"autoCreateTime:milli" json:"createdAt"`
+	DeletedAt int64     `gorm:"autoDeleteTime:milli" json:"deletedAt"`
 }
